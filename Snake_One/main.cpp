11c4c4bcf18b6b794cb87295ce618e86d8661329
Piotr <piotr.andrea.kozyra@gmail.com>
2017-11-08 16:58:29 +0100

    SSS

---
+++ Snake_One/main.cpp
@@ -0,0 +1,13 @@
+#include "MyForm.h" //linkujesz naglowek do forma
+
+using namespace SnakeOne; //tu wpisujesz nazwe projektu
+
+[STAThreadAttribute]
+int main()
+{
+
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	Application::Run(gcnew MyForm()); //podajesz nazwy forma
+	return 0;
+}
