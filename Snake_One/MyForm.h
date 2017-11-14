c7aa9041fbe3e95eaddad3a5c96bcf15a334bd19
Piotr <piotr.andrea.kozyra@gmail.com>
2017-11-14 16:36:25 +0100

    Zadanie2

--- Snake_One/MyForm.h
+++ Snake_One/MyForm.h
@@ -58,6 +58,7 @@ namespace SnakeOne {
 	private: System::Windows::Forms::Label^  label8;
 	private: System::Windows::Forms::Label^  label9;
 	private: System::Windows::Forms::PictureBox^  pictureBox1;
+	private: System::Windows::Forms::Label^  label10;
 	protected:
 
 	private:
@@ -73,6 +74,7 @@ namespace SnakeOne {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			System::ComponentModel::ComponentResourceManager^  resources = (gcnew System::ComponentModel::ComponentResourceManager(MyForm::typeid));
 			this->button1 = (gcnew System::Windows::Forms::Button());
 			this->button2 = (gcnew System::Windows::Forms::Button());
 			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
@@ -95,14 +97,15 @@ namespace SnakeOne {
 			this->label8 = (gcnew System::Windows::Forms::Label());
 			this->label9 = (gcnew System::Windows::Forms::Label());
 			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
+			this->label10 = (gcnew System::Windows::Forms::Label());
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
 			this->SuspendLayout();
 			// 
 			// button1
 			// 
-			this->button1->Location = System::Drawing::Point(30, 30);
+			this->button1->Location = System::Drawing::Point(12, 12);
 			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(75, 23);
+			this->button1->Size = System::Drawing::Size(57, 23);
 			this->button1->TabIndex = 0;
 			this->button1->Text = L"Przycisk";
 			this->button1->UseVisualStyleBackColor = true;
@@ -110,9 +113,9 @@ namespace SnakeOne {
 			// 
 			// button2
 			// 
-			this->button2->Location = System::Drawing::Point(159, 30);
+			this->button2->Location = System::Drawing::Point(75, 12);
 			this->button2->Name = L"button2";
-			this->button2->Size = System::Drawing::Size(75, 23);
+			this->button2->Size = System::Drawing::Size(63, 23);
 			this->button2->TabIndex = 1;
 			this->button2->Text = L"Przycisk2";
 			this->button2->UseVisualStyleBackColor = true;
@@ -120,9 +123,9 @@ namespace SnakeOne {
 			// 
 			// textBox1
 			// 
-			this->textBox1->Location = System::Drawing::Point(30, 120);
+			this->textBox1->Location = System::Drawing::Point(12, 54);
 			this->textBox1->Name = L"textBox1";
-			this->textBox1->Size = System::Drawing::Size(100, 20);
+			this->textBox1->Size = System::Drawing::Size(78, 20);
 			this->textBox1->TabIndex = 2;
 			this->textBox1->Text = L"Super Program";
 			this->textBox1->TextChanged += gcnew System::EventHandler(this, &MyForm::textBox1_TextChanged);
@@ -138,7 +141,7 @@ namespace SnakeOne {
 			// 
 			// button3
 			// 
-			this->button3->Location = System::Drawing::Point(30, 157);
+			this->button3->Location = System::Drawing::Point(12, 80);
 			this->button3->Name = L"button3";
 			this->button3->Size = System::Drawing::Size(75, 23);
 			this->button3->TabIndex = 4;
@@ -149,7 +152,7 @@ namespace SnakeOne {
 			// label2
 			// 
 			this->label2->AutoSize = true;
-			this->label2->Location = System::Drawing::Point(30, 202);
+			this->label2->Location = System::Drawing::Point(12, 143);
 			this->label2->Name = L"label2";
 			this->label2->Size = System::Drawing::Size(13, 13);
 			this->label2->TabIndex = 5;
@@ -157,7 +160,7 @@ namespace SnakeOne {
 			// 
 			// button4
 			// 
-			this->button4->Location = System::Drawing::Point(107, 197);
+			this->button4->Location = System::Drawing::Point(48, 138);
 			this->button4->Name = L"button4";
 			this->button4->Size = System::Drawing::Size(75, 23);
 			this->button4->TabIndex = 6;
@@ -167,67 +170,69 @@ namespace SnakeOne {
 			// 
 			// textBox2
 			// 
-			this->textBox2->Location = System::Drawing::Point(352, 32);
+			this->textBox2->Location = System::Drawing::Point(295, 28);
 			this->textBox2->Name = L"textBox2";
-			this->textBox2->Size = System::Drawing::Size(100, 20);
+			this->textBox2->Size = System::Drawing::Size(62, 20);
 			this->textBox2->TabIndex = 7;
+			this->textBox2->TextChanged += gcnew System::EventHandler(this, &MyForm::textBox2_TextChanged);
 			// 
 			// textBox3
 			// 
-			this->textBox3->Location = System::Drawing::Point(352, 58);
+			this->textBox3->Location = System::Drawing::Point(295, 54);
 			this->textBox3->Name = L"textBox3";
-			this->textBox3->Size = System::Drawing::Size(100, 20);
+			this->textBox3->Size = System::Drawing::Size(62, 20);
 			this->textBox3->TabIndex = 8;
 			// 
 			// button5
 			// 
-			this->button5->Location = System::Drawing::Point(352, 84);
+			this->button5->Location = System::Drawing::Point(243, 80);
 			this->button5->Name = L"button5";
-			this->button5->Size = System::Drawing::Size(36, 23);
+			this->button5->Size = System::Drawing::Size(33, 23);
 			this->button5->TabIndex = 9;
 			this->button5->Text = L"+";
 			this->button5->UseVisualStyleBackColor = true;
 			// 
 			// button6
 			// 
-			this->button6->Location = System::Drawing::Point(440, 84);
+			this->button6->Location = System::Drawing::Point(321, 80);
 			this->button6->Name = L"button6";
-			this->button6->Size = System::Drawing::Size(32, 23);
+			this->button6->Size = System::Drawing::Size(33, 23);
 			this->button6->TabIndex = 10;
-			this->button6->Text = L"*";
+			this->button6->Text = L"×";
 			this->button6->UseVisualStyleBackColor = true;
 			// 
 			// button7
 			// 
-			this->button7->Location = System::Drawing::Point(394, 84);
+			this->button7->Location = System::Drawing::Point(282, 80);
 			this->button7->Name = L"button7";
-			this->button7->Size = System::Drawing::Size(40, 23);
+			this->button7->Size = System::Drawing::Size(33, 23);
 			this->button7->TabIndex = 11;
 			this->button7->Text = L"-";
 			this->button7->UseVisualStyleBackColor = true;
 			// 
 			// button8
 			// 
-			this->button8->Location = System::Drawing::Point(479, 83);
+			this->button8->Location = System::Drawing::Point(360, 80);
 			this->button8->Name = L"button8";
-			this->button8->Size = System::Drawing::Size(28, 23);
+			this->button8->Size = System::Drawing::Size(33, 23);
 			this->button8->TabIndex = 12;
-			this->button8->Text = L"/";
+			this->button8->Text = L"÷";
 			this->button8->UseVisualStyleBackColor = true;
 			// 
 			// label3
 			// 
 			this->label3->AutoSize = true;
-			this->label3->Location = System::Drawing::Point(322, 35);
+			this->label3->Location = System::Drawing::Point(271, 32);
 			this->label3->Name = L"label3";
 			this->label3->Size = System::Drawing::Size(24, 13);
 			this->label3->TabIndex = 13;
 			this->label3->Text = L"x = ";
+			this->label3->Click += gcnew System::EventHandler(this, &MyForm::label3_Click);
 			// 
 			// label4
 			// 
 			this->label4->AutoSize = true;
-			this->label4->Location = System::Drawing::Point(322, 61);
+			this->label4->Location = System::Drawing::Point(271, 57);
 			this->label4->Name = L"label4";
 			this->label4->Size = System::Drawing::Size(24, 13);
 			this->label4->TabIndex = 14;
@@ -235,15 +240,15 @@ namespace SnakeOne {
 			// 
 			// textBox4
 			// 
-			this->textBox4->Location = System::Drawing::Point(352, 120);
+			this->textBox4->Location = System::Drawing::Point(292, 117);
 			this->textBox4->Name = L"textBox4";
-			this->textBox4->Size = System::Drawing::Size(100, 20);
+			this->textBox4->Size = System::Drawing::Size(65, 20);
 			this->textBox4->TabIndex = 15;
 			// 
 			// label5
 			// 
 			this->label5->AutoSize = true;
-			this->label5->Location = System::Drawing::Point(300, 123);
+			this->label5->Location = System::Drawing::Point(240, 120);
 			this->label5->Name = L"label5";
 			this->label5->Size = System::Drawing::Size(46, 13);
 			this->label5->TabIndex = 16;
@@ -252,25 +257,25 @@ namespace SnakeOne {
 			// label6
 			// 
 			this->label6->AutoSize = true;
-			this->label6->Location = System::Drawing::Point(391, 9);
+			this->label6->Location = System::Drawing::Point(300, 9);
 			this->label6->Name = L"label6";
-			this->label6->Size = System::Drawing::Size(55, 13);
+			this->label6->Size = System::Drawing::Size(54, 13);
 			this->label6->TabIndex = 17;
-			this->label6->Text = L"Zadanie 1";
+			this->label6->Text = L"Kalkulator";
 			// 
 			// label7
 			// 
 			this->label7->AutoSize = true;
-			this->label7->Location = System::Drawing::Point(391, 162);
+			this->label7->Location = System::Drawing::Point(585, 9);
 			this->label7->Name = L"label7";
-			this->label7->Size = System::Drawing::Size(52, 13);
+			this->label7->Size = System::Drawing::Size(47, 13);
 			this->label7->TabIndex = 18;
-			this->label7->Text = L"Zadanie2";
+			this->label7->Text = L"Obrazek";
 			// 
 			// label8
 			// 
 			this->label8->AutoSize = true;
-			this->label8->Location = System::Drawing::Point(391, 320);
+			this->label8->Location = System::Drawing::Point(328, 228);
 			this->label8->Name = L"label8";
 			this->label8->Size = System::Drawing::Size(52, 13);
 			this->label8->TabIndex = 19;
@@ -279,7 +284,7 @@ namespace SnakeOne {
 			// label9
 			// 
 			this->label9->AutoSize = true;
-			this->label9->Location = System::Drawing::Point(349, 344);
+			this->label9->Location = System::Drawing::Point(292, 250);
 			this->label9->Name = L"label9";
 			this->label9->Size = System::Drawing::Size(130, 13);
 			this->label9->TabIndex = 20;
@@ -287,17 +292,29 @@ namespace SnakeOne {
 			// 
 			// pictureBox1
 			// 
-			this->pictureBox1->Location = System::Drawing::Point(325, 178);
+			this->pictureBox1->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox1.Image")));
+			this->pictureBox1->Location = System::Drawing::Point(487, 32);
 			this->pictureBox1->Name = L"pictureBox1";
-			this->pictureBox1->Size = System::Drawing::Size(193, 118);
+			this->pictureBox1->Size = System::Drawing::Size(252, 250);
 			this->pictureBox1->TabIndex = 21;
 			this->pictureBox1->TabStop = false;
+			// 
+			// label10
+			// 
+			this->label10->AutoSize = true;
+			this->label10->Location = System::Drawing::Point(29, 120);
+			this->label10->Name = L"label10";
+			this->label10->Size = System::Drawing::Size(40, 13);
+			this->label10->TabIndex = 22;
+			this->label10->Text = L"Licznik";
+			this->label10->Click += gcnew System::EventHandler(this, &MyForm::label10_Click);
 			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(610, 383);
+			this->ClientSize = System::Drawing::Size(930, 383);
+			this->Controls->Add(this->label10);
 			this->Controls->Add(this->pictureBox1);
 			this->Controls->Add(this->label9);
 			this->Controls->Add(this->label8);
@@ -354,6 +371,12 @@ private: System::Void button4_Click(System::Object^  sender, System::EventArgs^ 
 	label2->Text = System::Convert::ToString(n);
 }
 private: System::Void MyForm_Load(System::Object^  sender, System::EventArgs^  e)
+{}
+private: System::Void label10_Click(System::Object^  sender, System::EventArgs^  e)
+{}
+private: System::Void label3_Click(System::Object^  sender, System::EventArgs^  e)
+{}
+private: System::Void textBox2_TextChanged(System::Object^  sender, System::EventArgs^  e)
 {}
 };
 }
