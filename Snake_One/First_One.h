a023a5d8849a7db5293ea1d11c742b2b89d05b23
Piotr <piotr.andrea.kozyra@gmail.com>
2017-11-22 20:19:33 +0100

    v1.3

--- Snake_One/First_One.h
+++ Snake_One/First_One.h
@@ -29,8 +29,6 @@ namespace SnakeOne
 			//
 			//TODO: W tym miejscu dodaj kod konstruktora
 			//
-
-
 		}
 
 	protected:
@@ -45,17 +43,9 @@ namespace SnakeOne
 			}
 		}
 
-
-	private: System::Windows::Forms::Button^  Prawo;
-	private: System::Windows::Forms::Button^  Lewo;
 	private: System::Windows::Forms::Timer^  Timer_lewo;
 	private: System::Windows::Forms::Timer^  Timer_prawo;
-
-
-	private: System::Windows::Forms::Button^  Gora;
-	private: System::Windows::Forms::Button^  Dol;
 	private: System::Windows::Forms::PictureBox^  Background;
-
 	private: System::Windows::Forms::Label^  label1;
 	private: System::Windows::Forms::PictureBox^  Head;
 	private: System::Windows::Forms::PictureBox^  Segment1;
@@ -67,9 +57,6 @@ namespace SnakeOne
 	private: System::Windows::Forms::Label^  Result;
 	private: System::Windows::Forms::Timer^  Timer_gora;
 	private: System::Windows::Forms::Timer^  Timer_dol;
-	private: System::Windows::Forms::Button^  Stop;
-
-
 	private: System::ComponentModel::IContainer^  components;
 
 	protected:
@@ -89,12 +76,8 @@ namespace SnakeOne
 		{
 			this->components = (gcnew System::ComponentModel::Container());
 			System::ComponentModel::ComponentResourceManager^  resources = (gcnew System::ComponentModel::ComponentResourceManager(First_One::typeid));
-			this->Prawo = (gcnew System::Windows::Forms::Button());
 			this->Timer_lewo = (gcnew System::Windows::Forms::Timer(this->components));
 			this->Timer_prawo = (gcnew System::Windows::Forms::Timer(this->components));
-			this->Lewo = (gcnew System::Windows::Forms::Button());
-			this->Gora = (gcnew System::Windows::Forms::Button());
-			this->Dol = (gcnew System::Windows::Forms::Button());
 			this->Background = (gcnew System::Windows::Forms::PictureBox());
 			this->label1 = (gcnew System::Windows::Forms::Label());
 			this->Head = (gcnew System::Windows::Forms::PictureBox());
@@ -107,24 +90,12 @@ namespace SnakeOne
 			this->Result = (gcnew System::Windows::Forms::Label());
 			this->Timer_gora = (gcnew System::Windows::Forms::Timer(this->components));
 			this->Timer_dol = (gcnew System::Windows::Forms::Timer(this->components));
-			this->Stop = (gcnew System::Windows::Forms::Button());
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->Background))->BeginInit();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->Head))->BeginInit();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->Segment1))->BeginInit();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->Segment2))->BeginInit();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->Apple))->BeginInit();
 			this->SuspendLayout();
-			// 
-			// Prawo
-			// 
-			this->Prawo->Enabled = false;
-			this->Prawo->Location = System::Drawing::Point(320, 228);
-			this->Prawo->Name = L"Prawo";
-			this->Prawo->Size = System::Drawing::Size(23, 23);
-			this->Prawo->TabIndex = 26;
-			this->Prawo->Text = L">";
-			this->Prawo->UseVisualStyleBackColor = true;
-			this->Prawo->Click += gcnew System::EventHandler(this, &First_One::Prawo_Click);
 			// 
 			// Timer_lewo
 			// 
@@ -135,39 +106,6 @@ namespace SnakeOne
 			// 
 			this->Timer_prawo->Interval = 200;
 			this->Timer_prawo->Tick += gcnew System::EventHandler(this, &First_One::Timer_prawo_Tick);
-			// 
-			// Lewo
-			// 
-			this->Lewo->Enabled = false;
-			this->Lewo->Location = System::Drawing::Point(262, 228);
-			this->Lewo->Name = L"Lewo";
-			this->Lewo->Size = System::Drawing::Size(23, 23);
-			this->Lewo->TabIndex = 27;
-			this->Lewo->Text = L"<";
-			this->Lewo->UseVisualStyleBackColor = true;
-			this->Lewo->Click += gcnew System::EventHandler(this, &First_One::Lewo_Click);
-			// 
-			// Gora
-			// 
-			this->Gora->Enabled = false;
-			this->Gora->Location = System::Drawing::Point(291, 200);
-			this->Gora->Name = L"Gora";
-			this->Gora->Size = System::Drawing::Size(23, 23);
-			this->Gora->TabIndex = 28;
-			this->Gora->Text = L"ʌ";
-			this->Gora->UseVisualStyleBackColor = true;
-			this->Gora->Click += gcnew System::EventHandler(this, &First_One::Gora_Click);
-			// 
-			// Dol
-			// 
-			this->Dol->Enabled = false;
-			this->Dol->Location = System::Drawing::Point(291, 256);
-			this->Dol->Name = L"Dol";
-			this->Dol->Size = System::Drawing::Size(23, 23);
-			this->Dol->TabIndex = 29;
-			this->Dol->Text = L"v";
-			this->Dol->UseVisualStyleBackColor = true;
-			this->Dol->Click += gcnew System::EventHandler(this, &First_One::Dol_Click);
 			// 
 			// Background
 			// 
@@ -184,9 +122,12 @@ namespace SnakeOne
 			// label1
 			// 
 			this->label1->AutoSize = true;
-			this->label1->Location = System::Drawing::Point(490, 19);
+			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 8.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+															  static_cast<System::Byte>(238)));
+			this->label1->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"label1.Image")));
+			this->label1->Location = System::Drawing::Point(465, 15);
 			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(94, 13);
+			this->label1->Size = System::Drawing::Size(110, 13);
 			this->label1->TabIndex = 30;
 			this->label1->Text = L"Wymiary: 600x300";
 			// 
@@ -229,9 +170,12 @@ namespace SnakeOne
 			// label2
 			// 
 			this->label2->AutoSize = true;
-			this->label2->Location = System::Drawing::Point(493, 36);
+			this->label2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 8.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+															  static_cast<System::Byte>(238)));
+			this->label2->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"label2.Image")));
+			this->label2->Location = System::Drawing::Point(465, 32);
 			this->label2->Name = L"label2";
-			this->label2->Size = System::Drawing::Size(89, 13);
+			this->label2->Size = System::Drawing::Size(104, 13);
 			this->label2->TabIndex = 34;
 			this->label2->Text = L"Segmenty: 20x20";
 			// 
@@ -251,9 +195,12 @@ namespace SnakeOne
 			// label3
 			// 
 			this->label3->AutoSize = true;
-			this->label3->Location = System::Drawing::Point(504, 53);
+			this->label3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 8.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+															  static_cast<System::Byte>(238)));
+			this->label3->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"label3.Image")));
+			this->label3->Location = System::Drawing::Point(478, 48);
 			this->label3->Name = L"label3";
-			this->label3->Size = System::Drawing::Size(71, 13);
+			this->label3->Size = System::Drawing::Size(83, 13);
 			this->label3->TabIndex = 36;
 			this->label3->Text = L"Japco: 20x20";
 			// 
@@ -264,7 +211,7 @@ namespace SnakeOne
 			this->Score->Font = (gcnew System::Drawing::Font(L"Segoe Print", 9.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 															 static_cast<System::Byte>(238)));
 			this->Score->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"Score.Image")));
-			this->Score->Location = System::Drawing::Point(15, 15);
+			this->Score->Location = System::Drawing::Point(15, 13);
 			this->Score->Margin = System::Windows::Forms::Padding(0);
 			this->Score->Name = L"Score";
 			this->Score->Size = System::Drawing::Size(50, 23);
@@ -278,7 +225,7 @@ namespace SnakeOne
 			this->Result->Font = (gcnew System::Drawing::Font(L"Segoe Print", 9.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 															  static_cast<System::Byte>(238)));
 			this->Result->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"Result.Image")));
-			this->Result->Location = System::Drawing::Point(60, 15);
+			this->Result->Location = System::Drawing::Point(60, 13);
 			this->Result->Margin = System::Windows::Forms::Padding(0);
 			this->Result->Name = L"Result";
 			this->Result->Size = System::Drawing::Size(49, 23);
@@ -295,33 +242,12 @@ namespace SnakeOne
 			this->Timer_dol->Interval = 200;
 			this->Timer_dol->Tick += gcnew System::EventHandler(this, &First_One::Timer_dol_Tick);
 			// 
-			// Stop
-			// 
-			this->Stop->AutoSize = true;
-			this->Stop->BackColor = System::Drawing::Color::Red;
-			this->Stop->BackgroundImageLayout = System::Windows::Forms::ImageLayout::None;
-			this->Stop->Enabled = false;
-			this->Stop->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 8.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
-															static_cast<System::Byte>(238)));
-			this->Stop->ForeColor = System::Drawing::Color::Transparent;
-			this->Stop->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"Stop.Image")));
-			this->Stop->Location = System::Drawing::Point(290, 228);
-			this->Stop->Margin = System::Windows::Forms::Padding(0);
-			this->Stop->Name = L"Stop";
-			this->Stop->Size = System::Drawing::Size(25, 24);
-			this->Stop->TabIndex = 39;
-			this->Stop->Text = L"X";
-			this->Stop->TextImageRelation = System::Windows::Forms::TextImageRelation::ImageAboveText;
-			this->Stop->UseVisualStyleBackColor = false;
-			this->Stop->Click += gcnew System::EventHandler(this, &First_One::Stop_Click);
-			// 
 			// First_One
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->BackColor = System::Drawing::SystemColors::Control;
 			this->ClientSize = System::Drawing::Size(600, 299);
-			this->Controls->Add(this->Stop);
 			this->Controls->Add(this->Result);
 			this->Controls->Add(this->Score);
 			this->Controls->Add(this->label3);
@@ -331,16 +257,16 @@ namespace SnakeOne
 			this->Controls->Add(this->Segment1);
 			this->Controls->Add(this->Head);
 			this->Controls->Add(this->label1);
-			this->Controls->Add(this->Dol);
-			this->Controls->Add(this->Gora);
-			this->Controls->Add(this->Lewo);
-			this->Controls->Add(this->Prawo);
 			this->Controls->Add(this->Background);
 			this->Icon = (cli::safe_cast<System::Drawing::Icon^>(resources->GetObject(L"$this.Icon")));
 			this->MaximizeBox = false;
 			this->Name = L"First_One";
 			this->Text = L"Snake";
 			this->Load += gcnew System::EventHandler(this, &First_One::First_One_Load);
+
+			// do sterowania klawiszami
+			this->KeyDown += gcnew System::Windows::Forms::KeyEventHandler(this, &First_One::First_One_KeyDown);
+
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->Background))->EndInit();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->Head))->EndInit();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->Segment1))->EndInit();
@@ -348,9 +274,6 @@ namespace SnakeOne
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->Apple))->EndInit();
 			this->ResumeLayout(false);
 			this->PerformLayout();
-
-			// do sterowania klawiszami
-			this->KeyDown += gcnew System::Windows::Forms::KeyEventHandler(this, &First_One::First_One_KeyDown);
 		}
 
 #pragma endregion Zawartosc formy
@@ -360,6 +283,8 @@ namespace SnakeOne
 		{
 			int x = Head->Location.X;
 			int y = Head->Location.Y;
+
+						// ruch manualny
 
 			//if (e->KeyCode == Keys::Right) x += 10;
 			//else if (e->KeyCode == Keys::Left) x -= 10;
@@ -404,8 +329,6 @@ namespace SnakeOne
 				Timer_dol->Enabled = false;
 				Timer_prawo->Enabled = false;
 			}
-
-			
 		}
 		
 				 // plansza jako pole do gry
@@ -413,56 +336,6 @@ namespace SnakeOne
 		{
 
 		}
-
-#pragma region Buttons
-		private: System::Void Lewo_Click(System::Object^  sender, System::EventArgs^  e) 
-		{
-			//if (Timer_lewo->Enabled)
-			//	Timer_lewo->Enabled = false;
-			//else
-				Timer_lewo->Enabled = true;
-				Timer_gora->Enabled = false;
-				Timer_dol->Enabled = false;
-				Timer_prawo->Enabled = false;
-		}
-		private: System::Void Prawo_Click(System::Object^  sender, System::EventArgs^  e)
-		{
-			//if (Timer_prawo->Enabled)
-			//	Timer_prawo->Enabled = false;
-			//else
-				Timer_prawo->Enabled = true;
-				Timer_lewo ->Enabled = false;
-				Timer_gora->Enabled = false;
-				Timer_dol->Enabled = false;
-		}
-		private: System::Void Gora_Click(System::Object^  sender, System::EventArgs^  e) 
-		{
-			//if (Timer_gora->Enabled)
-			//	Timer_gora->Enabled = false;
-			//else
-				Timer_gora->Enabled = true;
-				Timer_dol->Enabled = false;
-				Timer_prawo->Enabled = false;
-				Timer_lewo ->Enabled = false;
-		}
-		private: System::Void Dol_Click(System::Object^  sender, System::EventArgs^  e) 
-		{
-			//if (Timer_dol->Enabled)
-			//	Timer_dol->Enabled = false;
-			//else
-				Timer_dol->Enabled = true;
-				Timer_gora->Enabled = false;
-				Timer_prawo->Enabled = false;
-				Timer_lewo->Enabled = false;
-		}
-		private: System::Void Stop_Click(System::Object^  sender, System::EventArgs^  e)
-		{
-			Timer_dol->Enabled = false;
-			Timer_gora->Enabled = false;
-			Timer_prawo->Enabled = false;
-			Timer_lewo->Enabled = false;
-		}
-#pragma endregion Przyciski ruchu
 
 #pragma region Timery
 		private: System::Void Timer_lewo_Tick(System::Object^  sender, System::EventArgs^  e)
