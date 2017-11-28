d95948df4f6d0947f0242255f1f3693676f971a6
Piotr <piotr.andrea.kozyra@gmail.com>
2017-11-28 17:41:45 +0100

    nowa

--- Snake_One/waz.h
+++ Snake_One/waz.h
@@ -16,22 +16,8 @@ public:
 	void die();
 	//void waz();
 
-	waz()
-	{
-		segment* seg1, *tail;
-		head = new segment;
-		seg1 = new segment;
-		tail = new segment;
-		head->nastepny = seg1;
-		seg1->nastepny = tail;
-		tail->nastepny = 0;
-		head->x = 20;
-		head->y = 10;
-		seg1->x = 21;
-		seg1->y = 10;
-		tail->x = 22;
-		tail->y = 10;
-	};
-//	~waz();
+	waz();
+
+	~waz();
 };
 
